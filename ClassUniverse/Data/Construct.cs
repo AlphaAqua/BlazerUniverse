531525197c7701918e8dfa34e1119e3cd1255f7e
testuser <temp@test.com>
2023-09-10 04:54:48 -0400

    work

---
+++ ClassUniverse/Data/Construct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassUniverse.Data
+{
+  internal class Construct
+  {
+    public Sector? Sector { get; }
+
+  }
+}
---
+++ ClassUniverse/Services/UniverseService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassUniverse.Data;
+
+namespace ClassUniverse.Services
+{
+  internal class UniverseService
+  {
+    private Universe? defaultUniverse;
+
+    public Universe NewUniverse ()
+    {
+      var uni = new Universe
+      {
+        Name = "Galaxia"
+      };
+
+      defaultUniverse = uni;
+      return uni;
+    }
+
+    public void RunUniverseTick ()
+    {
+
+    }
+  }
+}
