f209cf3521eb65255ceab82596541e7019c2e9d2
testuser <temp@test.com>
2023-09-10 04:54:48 -0400

    work

---
+++ ClassUniverse/Data/Universe.cs
@@ -0,0 +1,8 @@
+﻿namespace ClassUniverse.Data
+{
+    public class Universe
+    {
+        public string Name { get; set; } = "";
+        public List<Sector> Sectors { get; set; } = new List<Sector>();
+    }
+}
---
+++ ClassUniverse/Services/UniverseService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassUniverse.Data;
+
+namespace ClassUniverse.Services
+{
+  internal class UniverseService
+  {
+    private Universe? defaultUniverse;
+
+    public Universe NewUniverse ()
+    {
+      var uni = new Universe
+      {
+        Name = "Galaxia"
+      };
+
+      defaultUniverse = uni;
+      return uni;
+    }
+
+    public void RunUniverseTick()
+    {
+      if (defaultUniverse == null)
+      {
+        throw new ArgumentNullException("No universe to run exist");
+      }
+      RunUniverseTick(defaultUniverse);
+    }
+
+    public void RunUniverseTick (Universe universe)
+    {
+      if (universe == null)
+      {
+        throw new ArgumentNullException(nameof(universe));
+      }
+    }
+  }
+}
